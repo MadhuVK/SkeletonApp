71962987de5124250088b93abf764b2be394c020
Madhu Krishnan <MadhuV.Krishnan@gmail.com>
2013-09-22 11:35:15 -0700

    Initialization

---
+++ README.md
@@ -0,0 +1,1 @@
+Skeleton App for future reference
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PracticeApp::Application.config.secret_key_base = 'a3e0dcc11e15b7869b7dc1f64e676d5a349ff0f67ec55886a44c441a588d1d35322b82801645b77864a6be9b664685fba0a4385b62186e4fa18b44694f97f545'
